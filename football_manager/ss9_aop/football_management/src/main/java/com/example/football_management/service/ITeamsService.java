bc8b1309495c28162f89ff0403c42fe1aaea8147
tnq112 <79589849+tnq112@users.noreply.github.com>
2023-04-07 16:54:45 +0700

    update_ss9_aop

---
+++ football_manager/ss9_aop/football_management/src/main/java/com/example/football_management/service/ITeamsService.java
@@ -0,0 +1,9 @@
+package com.example.football_management.service;
+
+import com.example.football_management.model.Teams;
+
+import java.util.List;
+
+public interface ITeamsService {
+    List<Teams> findAll();
+}
